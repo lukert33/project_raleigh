297f12d0ea827ec78fcda488f743005f7d6c581f
Luke Thomas <Lukert33@gmail.com>
2015-01-02 17:41:08 -0500

    Build page model.

---
+++ app/models/page.rb
@@ -0,0 +1,8 @@
+class Page < ActiveRecord::Base
+  has_many :user_bookmarks, class_name: 'User', foreign_key: 'current_page_id'
+  has_one  :challenge
+  belongs_to :next_page, class_name: 'Page'
+  has_one    :prev_page, class_name: 'Page', foreign_key: 'next_page_id'
+  has_many :success_routes, class_name: 'Challenge', foreign_key: 'success_page_id'
+  has_many :fail_routes, class_name: 'Challenge', foreign_key: 'fail_page_id'
+end
