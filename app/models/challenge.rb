3e425654080b64186e051a27d1f58689f69a56d0
Luke Thomas <Lukert33@gmail.com>
2015-01-02 18:52:48 -0500

    Create methods to check users and challenges through join table.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,4 +3,5 @@ class Challenge < ActiveRecord::Base
   belongs_to :success_page, class_name: 'Page'
   belongs_to :fail_page, class_name: 'Page'
   has_many   :user_challenges
+  has_many   :users, through: :user_challenges
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   before_create :set_page_one
   belongs_to :current_page, class_name: 'Page'
   has_many :user_challenges
+  has_many :challenges, through: :user_challenges
 
   def set_page_one
     self.current_page_id = 1
