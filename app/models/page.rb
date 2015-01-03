d6bba60c69d0a523c78c26afffe4cee30528b2b9
Luke Thomas <Lukert33@gmail.com>
2015-01-02 20:45:37 -0500

    Add UserPage model.

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,4 +5,5 @@ class Page < ActiveRecord::Base
   has_one    :prev_page, class_name: 'Page', foreign_key: 'next_page_id'
   has_many :success_routes, class_name: 'Challenge', foreign_key: 'success_page_id'
   has_many :fail_routes, class_name: 'Challenge', foreign_key: 'fail_page_id'
+  has_many :user_pages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   belongs_to :farthest_page, class_name: 'Page'
   has_many :user_challenges
   has_many :challenges, through: :user_challenges
+  has_many :user_pages
+  has_many :read_pages, through: :user_pages, source: :page
 
   def set_page_one
     self.farthest_page_id = 1
