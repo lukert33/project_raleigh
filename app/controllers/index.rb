977e9d39161f08e6c60843d0684a77679ac3b29a
Luke Thomas <Lukert33@gmail.com>
2015-01-04 16:35:21 -0500

    Move user routes to index controller because they are closely related.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,6 +9,34 @@ get '/' do
   end
 end
 
+get '/user/new' do
+  if request.xhr?
+    erb :'signup', layout:false
+  else
+    erb :'signup'
+  end
+end
+
+get '/user/:id' do |id|
+  @user = User.find(id)
+  @page = User.farthest_page
+  if request.xhr?
+    erb :'page', layout:false
+  else
+    erb :'page'
+  end
+end
+
+post '/user' do
+  @user = User.create(params[:user])
+  @page = User.farthest_page
+  if request.xhr?
+    erb :"page", layout:false
+  else
+    erb :"page"
+  end
+end
+
 
 
 
