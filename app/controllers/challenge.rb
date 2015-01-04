86c7f9048f7ed27d10281b1e5026be30fd2da50b
Luke Thomas <Lukert33@gmail.com>
2015-01-04 14:16:06 -0500

    Wrote routes for navigating to success or fail page after resolution of challenge.

--- app/controllers/challenge.rb
+++ app/controllers/challenge.rb
@@ -7,6 +7,16 @@ get '/challenge/:id' do |id|
   end
 end
 
-post '/challenge/user_challenge/:id' |id|
+post '/challenge/user_challenge/:id' do |id|
   UserChallenge.create(params[:user_challenge])
+end
+
+get '/challenge/result/:id' do |id|
+  user_challenge = UserChallenge.find(id)
+  if user_challenge.success
+    @page = user_challenge.success_page
+  else
+    @page = user_challenge.fail_page
+  end
+  redirect "/page/#{@page.id}"
 end
