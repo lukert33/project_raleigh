7dcf4f410a1bb25ca005bdd1542d1a11ac1c230a
Luke Thomas <Lukert33@gmail.com>
2015-01-04 14:20:07 -0500

    Add validations to UserPage with method to update farthest page on creation of new entry.

--- app/models/user_page.rb
+++ app/models/user_page.rb
@@ -1,4 +1,12 @@
 class UserPage < ActiveRecord::Base
+  validates :page_id, :user_id, presence: true
+  before_save :update_user_bookmark
   belongs_to :user
   belongs_to :page
+
+  def update_user_bookmark
+    u_id = self.user_id
+    p_id = self.page_id
+    User.find(u_id).update(farthest_page_id: p_id)
+  end
 end
