967cac58e2c3e4e83d822600f79ee8a40f48c02b
Luke Thomas <Lukert33@gmail.com>
2015-01-04 17:53:16 -0500

    Drafted modular jquery post behavior.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#content").on("submit", "form.save-form", function(event){
+    event.preventDefault();
+    var $target = $(event.target)
+    var entryType = target.attr("entry-type")
+    $.ajax({
+      url: $target.attr("action"),
+      type: $target.attr("method"),
+      data: $target.serialize()
+    }).done(function(response){
+      console.log("saved new "+entrType);
+      //append to indicated element
+    }).fail(function(response){
+      console.log("failure: "+response)
+    }).always(function(){
+      $target.parent().toggle("slow")
+    })
+  })
 });
