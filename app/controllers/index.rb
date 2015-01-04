352afed95e937cf53c30c8fa6570f5dd0d0ec9f9
Luke Thomas <lukert33@gmail.com>
2015-01-03 19:28:53 -0500

    Drafted welcome route.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,13 @@
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  if !session[:user_id]
+    erb :index
+  else
+    @user = User.find(session[:user_id])
+    @page = Page.find(@user.farthest_page.id)
+    erb :"page"
+  end
 end
+
+
+
+
