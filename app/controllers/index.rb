1ba5e73e147622d7d86c447274da0c980b4c6c29
Luke Thomas <lukert33@gmail.com>
2015-01-03 20:09:37 -0500

    Draft session read for user at home page.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,10 @@
 get '/' do
+  p "user_id #{session[:user_id]}"
   if !session[:user_id]
     erb :index
   else
     @user = User.find(session[:user_id])
-    @page = Page.find(@user.farthest_page.id)
+    @page = @user.farthest_page
     erb :"page"
   end
 end
