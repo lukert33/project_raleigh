5da4697a61c3439d460c2961a26fce10bad20d69
Luke Thomas <Lukert33@gmail.com>
2015-01-06 18:49:46 -0500

    Wrote page specs that pass.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,21 +5,23 @@ describe User do
   let(:valid_user){User.find(1)}
 
   it "has a username and password" do
-    expect(valid_user.username).to be_truthy
-    expect(valid_user.password).to be_truthy
+    p valid_user
+    expect(valid_user.username).to be_a String
+    expect(valid_user.password_digest).to be_a String
   end
 
   it "has methods for challenge lookup" do
-    expect(valid_user.methods.include(challenges)).to_be true
+    expect(valid_user.challenges[0]).to be_a Challenge
   end
 
   it "has methods for UserPage lookup" do
-    expect(valid_user.methods.include(farthest_page)).to_be true
-    expect(valid_user.methods.include(read_pages)).to_be true
+    expect(valid_user.farthest_page).to be_a Page
+    expect(valid_user.read_pages.first).to be_a Page
+    expect(valid_user.read_pages.last).to eq(valid_user.farthest_page)
   end
 
   it "has a farthest_page_id of at least 1" do
-    expect(valid_user.farthest_page).to_be >= 1
+    expect(valid_user.farthest_page.id >= 1).to be true
   end
 
 end
