6b01b8d2db4f44c620134ba4b78409d25a08b717
Luke Thomas <Lukert33@gmail.com>
2015-01-06 18:48:30 -0500

    Add boolean queries to Page model, removed unneeded database row.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,17 @@
 class Page < ActiveRecord::Base
   has_one  :challenge
   belongs_to :next_page, class_name: 'Page'
-  has_one    :prev_page, class_name: 'Page', foreign_key: 'next_page_id'
+  has_many    :prev_pages, class_name: 'Page', foreign_key: 'next_page_id'
   has_many :success_routes, class_name: 'Challenge', foreign_key: 'success_page_id'
   has_many :fail_routes, class_name: 'Challenge', foreign_key: 'fail_page_id'
   has_many :user_pages
+
+  def has_challenge?
+    !!challenge
+  end
+
+  def leads_somewhere?
+    has_challenge? || next_page != nil
+  end
+
 end
--- db/migrate/20150102163313_create_pages.rb
+++ db/migrate/20150102163313_create_pages.rb
@@ -2,7 +2,6 @@ class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
       t.text :text
-      t.boolean :has_challenge
       t.references :next_page
       t.timestamps
     end
