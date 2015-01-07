23098a5290357d1e580b5dbb31aefaf0e0d38a16
Luke Thomas <Lukert33@gmail.com>
2015-01-07 17:12:27 -0500

    Refactor to move POST functions of new next page movement to POST route; decuttered GET page/:id route.

--- app/controllers/page.rb
+++ app/controllers/page.rb
@@ -1,9 +1,6 @@
 get '/page/:id' do |id|
   @user = current_user
   @page = Page.find(id)
-  unless @user.read_pages.include?(@page)
-    UserPage.create(user_id: @user.id, page_id: @page.id)
-  end
   if request.xhr?
     erb :'page', layout:false
   else
@@ -11,12 +8,14 @@ get '/page/:id' do |id|
   end
 end
 
-get '/page/:id/next' do |id|
-  page = Page.find(id)
-  if page.has_challenge?
-    redirect "/challenge/:id"
+post '/pages/:id/next' do |id|
+  @user = current_user
+  @page = Page.find(id)
+  if @page.has_challenge?
+    redirect "/challenge/#{@page.challenge.id}"
   else
-    redirect "/page/#{page.next_page_id}"
+    UserPage.find_or_create_by(user_id: @user.id, page_id: @page.next_page_id)
+    redirect "/page/#{@page.next_page_id}"
   end
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+  $(".content").on("click", ".turn-page", function(event){
+    event.preventDefault()
+    var $target = $(event.target).closest("a")
+    $.ajax({
+      url: $target.attr("href")
+    }).done(function(response){
+      console.log("turned a page")
+      $(".content").empty().append(response)
+    }).fail(function(){
+      console.log("GET page/challenge error")
+    })
+  })
+
+  $(".content").on("click", "#reveal-next", function(event){
+    event.preventDefault()
+    $("#next-page").submit();
+  })
 
 });
