1b37b4aa5200d87c8106a4ba066a52771f982cbb
Luke Thomas <Lukert33@gmail.com>
2015-01-07 13:11:14 -0500

    Refactor so that all auth forms follow modular script for AJAX call; Pay attention to deploying the appendTo attribute in POST requests.

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,19 +1,58 @@
 $(document).ready(function(){
-  $('.content').on("submit", "form.signup", function(event){
+
+  $(".content").on("submit", "form.get-form", function(event){
     event.preventDefault()
     var $target = $(event.target)
+    var appendTo = $target.attr("appendTo")
+    $.ajax({
+      url: $target.attr("action"),
+      type: "GET",
+    }).always(function(){
+      $target.children("input").prop("disabled",true)
+    }).done(function(response){
+      console.log("done GET")
+      $target.closest(appendTo).replaceWith(response)
+    }).fail(function(){
+      console.log("GET request failed")
+    })
+  })
+
+  $(".content").on("submit", "form.auth-form", function(event){
+    event.preventDefault()
+    var $target = $(event.target)
+    var appendTo = $target.attr("appendTo")
     $.ajax({
       url: $target.attr("action"),
       type: $target.attr("method"),
       data: $target.serialize()
+    }).done(function(response){
+      console.log("done POST")
+      debugger
+      $target.closest(appendTo).empty().append(response)
+      $("#logout").toggle("600")
+    }).fail(function(){
+      console.log("POST failed")
     }).always(function(){
       $target.children("input").prop("disabled",true)
-    }).done(function(response){
-      console.log("done")
-      $target.closest(".auth-box").empty().append(response)
-    }).fail(function(){
-      console.log("request failed")
     })
   })
+
+  // $('.content').on("submit", "form.signup", function(event){
+  //   event.preventDefault()
+  //   var $target = $(event.target)
+  //   $.ajax({
+  //     url: $target.attr("action"),
+  //     type: $target.attr("method"),
+  //     data: $target.serialize()
+  //   }).always(function(){
+  //     $target.children("input").prop("disabled",true)
+  //   }).done(function(response){
+  //     console.log("done")
+  //     $target.closest(".auth-box").empty().append(response)
+  //     $("nav").children().toggle("slow")
+  //   }).fail(function(){
+  //     console.log("request failed")
+  //   })
+  // })
 
 })
