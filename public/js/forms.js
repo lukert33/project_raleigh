3954fac146e93fe5c623c4be33c7f6c97cbaf56c
Luke Thomas <Lukert33@gmail.com>
2015-01-07 20:32:08 -0500

    Remove old script

--- public/js/forms.js
+++ public/js/forms.js
@@ -35,22 +35,4 @@ $(document).ready(function(){
     })
   })
 
-  // $('.content').on("submit", "form.signup", function(event){
-  //   event.preventDefault()
-  //   var $target = $(event.target)
-  //   $.ajax({
-  //     url: $target.attr("action"),
-  //     type: $target.attr("method"),
-  //     data: $target.serialize()
-  //   }).always(function(){
-  //     $target.children("input").prop("disabled",true)
-  //   }).done(function(response){
-  //     console.log("done")
-  //     $target.closest(".auth-box").empty().append(response)
-  //     $("nav").children().toggle("slow")
-  //   }).fail(function(){
-  //     console.log("request failed")
-  //   })
-  // })
-
 })
